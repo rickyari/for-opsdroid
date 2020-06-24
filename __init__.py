04e7ab8a0a4bb4abe9fa18248fae63c99c3e26ca
Gagandeep Singh <gagsingh@expediagroup.com>
2020-06-24 20:53:13 +0530

    removed raw string

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from opsdroid.matchers import match_regex
 class MyFirstSkill(Skill):
     '''My first test skill using github for opsdroid'''
 
-    @match_regex(r'how are you\??')
+    @match_regex('how are you?')
     async def how_are_you(self, message):
         await message.respond("Good thanks!, How are you sir")
 
