79108813eb717e4abf639b187c5d549e68ec3967
Gagandeep Singh <gagsingh@expediagroup.com>
2020-06-24 20:23:26 +0530

    first test skill

---
+++ __init__.py
@@ -0,0 +1,11 @@
+
+from opsdroid.skill import Skill
+from opsdroid.matchers import match_regex
+
+class MyFirstSkill(Skill):
+    '''My first test skill using github for opsdroid'''
+
+    @match_regex(r'how are you\??')
+    async def how_are_you(self, message):
+        await message.respond("Good thanks!, How are you sir")
+
